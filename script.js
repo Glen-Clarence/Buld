323ab035496dc92e4456772f885bad9b35c57431
Glen Clarence <glenclarence007@gmail.com>
2022-07-11 00:38:50 +0530

    added some nav-links animation

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 gsap.from('header',{opacity:0, duration:0.5, y:-70,delay:5.5});
+gsap.from('.nav-link',{opacity:0, duration:0.5,x:-20, delay:6, stagger:0.3});
 gsap.from('.first-block',{opacity:0, duration:2, y:-70, ease: "elastic.out(1, 0.3)",delay:5});
 gsap.from('.second-block',{opacity:0, duration:1,delay:5.4, stagger: 0.6});
 gsap.from('.hero-image',{opacity:0,duration:1, y: -100,ease: "expo.out",delay:5});
